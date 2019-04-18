d5101e1020cefc8cc8b4b730cb1e0d047e9eb31f
Zlatian Iliev <zlatian.iliev@oneclipboard.com>
2019-04-18 21:05:21 +0200

    Add a solution for the Reverse A String problem

---
+++ reverseString.js
@@ -0,0 +1,9 @@
+/**
+ * Write a function that takes a string as input and return it's reverse.
+ */
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const reverseString = s => s.split('').reverse().join('');
